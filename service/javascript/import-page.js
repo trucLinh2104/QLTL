4e91dcd21fbdaac3c752123a9b82828a66d90d07
0100865898 <luutruclinh@cathaylife.com.vn>
2025-05-07 08:27:50 +0700

    create import-bill-page

--- service/javascript/import-page.js
+++ service/javascript/import-page.js
@@ -1,3 +1,22 @@
 document.querySelector('input[type="date"]').addEventListener('click', function() {
     this.showPicker();  // Mở trình chọn ngày
 });
+
+document.querySelector('.grid-container').addEventListener('click', function(e) {
+    if (e.target.classList.contains('delete-btn')) {
+        let bill_no = e.target.getAttribute('data-bill_no');
+        alert('XÓA dòng');
+        console.log(bill_no);
+
+    }
+    if (e.target.classList.contains('edit-btn')) {
+        let bill_no = e.target.getAttribute('data-bill_no');
+        alert('SỬA dòng');
+        console.log(bill_no);
+    }
+});
+
+document.querySelector('.button-import').addEventListener('click', function(e) {
+    e.preventDefault()
+    window.location.href = 'import-bill-page.html';
+})
